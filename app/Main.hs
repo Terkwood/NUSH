c6ffe925f858d3b66832ba1a16dd04db595b7b84
terkwood <38859656+Terkwood@users.noreply.github.com>
2021-04-17 17:07:01 -0400

    feat: add ownership, refactor Object

--- app/Main.hs
+++ app/Main.hs
@@ -2,5 +2,7 @@ module Main where
 
 import Lib
 
+wiz = Player Attrs {Lib.id = DbRef 1, owner = Self}
+
 main :: IO ()
-main = print (Player Attrs {Lib.id = DbRef 0})
+main = print (PLAYER wiz)
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,11 +2,25 @@ module Lib
   ( Object (..),
     Attrs (..),
     DbRef (..),
+    Player (..),
+    Owner (..),
   )
 where
 
-data Object = Player Attrs | Room Attrs | Thing Attrs | Exit Attrs | Trash Attrs deriving (Show)
+data Object = PLAYER Player | ROOM Room | THING Thing | EXIT Exit | TRASH Trash deriving (Show)
 
-newtype Attrs = Attrs {id :: DbRef} deriving (Show)
+newtype Player = Player Attrs deriving (Show)
+
+newtype Room = Room Attrs deriving (Show)
+
+newtype Thing = Thing Attrs deriving (Show)
+
+newtype Exit = Exit Attrs deriving (Show)
+
+newtype Trash = Trash Attrs deriving (Show)
+
+data Attrs = Attrs {id :: DbRef, owner :: Owner} deriving (Show)
+
+data Owner = Self | Other Player deriving (Show)
 
 newtype DbRef = DbRef Word deriving (Show)
